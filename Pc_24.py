7fbacc4108b1465300b32249a781cb7b302cc9a3
1544717589@qq.com <Zx8331102>
2020-07-23 16:17:33 +0800

    项目实战之单个功能操作实现二

--- Pc_24.py
+++ Pc_24.py
@@ -7,28 +7,35 @@ import pywinauto
 from pywinauto import mouse
 import time
 
-app = pywinauto.Application("uia").start(r"D:\navicat112_premium_cs_x64\Navicat Premium\navicat.exe")
-app = pywinauto.Application(backend='uia').connect(process=14928)
+# app = pywinauto.Application("uia").start(r"D:\navicat112_premium_cs_x64\Navicat Premium\navicat.exe")
+app = pywinauto.Application(backend='uia').connect(process=1556)
+time.sleep(1)
 
 # 通过窗口标题去选择窗口
 dlg = app["Navicat Premium"]
 # dlg.print_control_identifiers()
 
-# # 选择树状视图控件 TVirtualStringTree -找不到这个控件
-# dlg['TTreeView'].print_control_identifiers()
+# 选择菜单
+menu = dlg["menu"]
+# menu.print_control_identifiers()
 
+# # 选择树状视图控件 TVirtualStringTree -找不到这个控件 TVTFilterFrame.TPane.TNavicatMainForm
+# dlg["TVTFilterFrame"].child_window(class_name="TVirtualStringTree").print_control_identifiers()
 
-# db_name = dlg["TVirtualStringTree"].child_window(title='test1', control_type='TreeItem')
-# # 获取数据库连接test1 中心点的位置 mid_point
+# 方式一：
+# db_name = dlg["TVTFilterFrame"].TVirtualStringTree
+# #  获取数据库连接test1 中心点的位置 mid_point
 # rect = db_name.rectangle().mid_point()
 # print(rect.x, rect.y)
-# 鼠标在控件中心点，右击
+# # 鼠标在控件中心点，右击
 # mouse.right_click(coords=(rect.x, rect.y))
 
-# 找不到树状视图控件,只能使用绝对坐标然后点击
+
+# 方式二：找不到树状视图控件,只能使用绝对坐标然后点击
 # mouse.right_click(coords=(rect.x, rect.y))
 time.sleep(2)
-mouse.right_click(coords=(151, 325))
+mouse.right_click(coords=(56, 213))
+
 
 # 获取该应用程序所有的窗口
 # print(app.window())
